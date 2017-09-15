3c01fb46a008bb817f12834ac083fe149725a67f
alpoxo <ebernhaut@itbernhaut.ch>
2017-09-16 00:36:51 +0200

    Added redux / redux thunk

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import departureReducer from "./departures/reducer";
+import Departures from "./departures/Departures";
+
+const rootReducer = combineReducers({
+    departures: departureReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Departures />
+            </Provider>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
