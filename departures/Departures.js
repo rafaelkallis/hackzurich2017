99ff5ebfd62dd02b59ac5d1d8cd273216d59d108
alpoxo <ebernhaut@itbernhaut.ch>
2017-09-16 11:36:01 +0200

    Change listview to swipe view with favourite

--- departures/Departures.js
+++ departures/Departures.js
@@ -9,7 +9,59 @@ import {
 } from "react-native";
 import { connect } from "react-redux";
 import { getCloseDepartures } from "./actions";
+import SwipeList from "react-native-smooth-swipe-list";
 import Checkpoints from "../checkpoints/Checkpoints";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const toggleFavourite = async id => {
+    const favourites = [];
+    const isFavourite = false;
+    const favouritesJSON = await AsyncStorage.getItem("favourites");
+
+    if (favouritesJSON) favourites = JSON.parse(favouritesJSON);
+
+    if (!(isFavourite = favourites.includes(id))) favourites.push(id);
+    else favourites.filter(fav => fav !== id);
+
+    await AsyncStorage.setItem("favourites", JSON.stringify(favourites));
+
+    return isFavourite;
+};
+
+const departureRowData = ({
+    favourite,
+    from,
+    to,
+    category,
+    number,
+    departureTime,
+}) => ({
+    id: `${from.name}_${category}_${number}_${departureTime}`,
+    rightSubView: (
+        <View style={styles.rightSubView}>
+            <Text>
+                <Icon
+                    name={favourite ? "star" : "star-o"}
+                    size={20}
+                    color="gold"
+                />
+            </Text>
+        </View>
+    ),
+    rowView: (
+        <Departure
+            key={`${from.name}_${category}_${number}_${departureTime}`}
+            {...{
+                from,
+                to,
+                category,
+                number,
+                departureTime,
+            }}
+        />
+    ),
+    style: styles.row,
+});
 
 export default connect(state => ({
     pending: state.departures.pending,
@@ -33,20 +85,7 @@ export default connect(state => ({
                     }
                     style={styles.container}
                 >
-                    {departures.map(
-                        ({ from, to, category, number, departureTime }) => (
-                            <Departure
-                                key={`${from.name}_${category}_${number}_${departureTime}`}
-                                {...{
-                                    from,
-                                    to,
-                                    category,
-                                    number,
-                                    departureTime,
-                                }}
-                            />
-                        ),
-                    )}
+                    <SwipeList rowData={departures.map(departureRowData)} />
                 </ScrollView>
             );
         }
@@ -55,6 +94,17 @@ export default connect(state => ({
 
 const styles = StyleSheet.create({
     container: {
+        height: "100%",
+    },
+    row: {
+        backgroundColor: "white",
+    },
+    rightSubView: {
+        flex: -1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "steelblue",
+        width: 80,
         height: "100%",
     },
 });
